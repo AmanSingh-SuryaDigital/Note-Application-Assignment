8e7c6f8dc99e3ea2fe611d40257d877273323a8e
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-20 15:42:48 +0530

    Added Note List

---
+++ src/main/java/org/example/NoteList.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Comparator;
+
+
+public class NoteList {
+
+    public static String toGetNote() {
+        System.out.println("List of note");
+        File folder = new File(Main.folder);
+        Scanner scanner = new Scanner(System.in);
+
+        File[] files = folder.listFiles();
+
+        if (files == null) {
+            System.out.println("No files found in the directory or directory does not exist.");
+            return null;
+        }
+
+        ArrayList<File> fileList = new ArrayList<>();
+        for (File file : files) {
+            if (file.isFile()) {
+                fileList.add(file);
+            }
+        }
+
+        DateCompare dateCompare = new DateCompare();
+        Collections.sort(fileList, dateCompare);
+
+        int count = 0;
+
+        for (File file : fileList) {
+            System.out.println(++count + ". " + file.getName());
+        }
+        System.out.println("Enter the note number");
+        int userInput = scanner.nextInt();
+
+        return fileList.get(userInput - 1).getName();
+    }
+
+    public static void toDisplayNoteList() {
+
+        File folder = new File(Main.folder);
+
+        File[] files = folder.listFiles();
+        if (files == null) {
+            System.out.println("No files found in the directory or directory does not exist.");
+            return;
+        }
+
+        List<File> fileList = new ArrayList<>();
+        for (File file : files) {
+            fileList.add(file);
+        }
+
+        DateCompare dateCompare = new DateCompare();
+        Collections.sort(fileList, dateCompare);
+
+        System.out.println("List of File");
+        int count = 0;
+
+        for (File file : fileList) {
+            System.out.println(++count + ". " + file.getName());
+        }
+    }
+
+    public static class DateCompare implements Comparator<File> {
+
+        @Override
+        public int compare(File f1, File f2) {
+            try {
+                FileTime creationTime1 = Files.readAttributes(f1.toPath(), BasicFileAttributes.class).creationTime();
+                FileTime creationTime2 = Files.readAttributes(f2.toPath(), BasicFileAttributes.class).creationTime();
+                return creationTime2.compareTo(creationTime1);
+            } catch (IOException e) {
+                System.err.println("Error comparing files: " + e.getMessage());
+                return 0;
+            }
+        }
+    }
+}
