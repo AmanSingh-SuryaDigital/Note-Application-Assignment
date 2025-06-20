0d89153e37401ab85ccc088c499baa2fa95296ec
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-20 15:37:18 +0530

    Added create note with util class

---
+++ src/main/java/org/example/CreateNote.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CreateNote {
+
+    public static void toCreateNote() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the Note Title");
+        String noteTitle = scanner.next();
+        if (Utils.inputValidation(noteTitle)) {
+            System.out.println(" This is invalid Input ");
+            return;
+        }
+
+        File file = new File(Main.folder, noteTitle);
+        try {
+            if (file.createNewFile()) {
+                System.out.println("Note file is Created with title " + noteTitle);
+            } else {
+                System.out.println("This Note File is already exits. ");
+                return;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Write the body of the note");
+        System.out.println("and Type 'Exit' on a new line to finish");
+
+        StringBuilder bodyOfNote = new StringBuilder();
+
+        while (true) {
+            String line = scanner.nextLine();
+
+            if (line.trim().equalsIgnoreCase("Exit")) {
+                break;
+            }
+
+            bodyOfNote.append(line).append("\n");
+        }
+
+        BufferedWriter bufferedWriter = null;
+
+        try {
+
+            bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(bodyOfNote.toString());
+
+            System.out.println("Note is saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Exception in saving note: " + e.getMessage());
+        } finally {
+
+            try {
+
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
+
+
---
+++ src/main/java/org/example/Utils.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.regex.Pattern;
+
+public class Utils {
+
+    public static boolean inputValidation(String input) {
+        String pattern = "[a-zA-Z0-9]{4,20}";
+        return !Pattern.matches(pattern, input);
+    }
+
+}
